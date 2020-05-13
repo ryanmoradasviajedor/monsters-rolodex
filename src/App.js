b2fa409adde21cf6290cd0600f3fd8ca0e170377
Ryan Moradas Viajedor <viajedor.ryan1990@gmail.com>
2020-05-13 21:24:59 +0800

    Added SearchField State

--- src/App.js
+++ src/App.js
@@ -7,19 +7,25 @@ class App extends Component {
     super();
 
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: '',
     };
   }
 
   componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
-    .then(response => response.json())
-    .then(users => this.setState({ monsters: users }));
+      .then((response) => response.json())
+      .then((users) => this.setState({ monsters: users }));
   }
 
   render() {
     return (
       <div className="App">
+        <input
+          type="search"
+          placeholder="search monsters"
+          onChange={(e) => this.setState({ searchField: e.target.value })}
+        />
         <CardList monsters={this.state.monsters} />
       </div>
     );
