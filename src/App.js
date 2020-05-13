29773f0d879caa34b492952987a37e53141e0032
Ryan Moradas Viajedor <viajedor.ryan1990@gmail.com>
2020-05-13 18:41:36 +0800

    Fetching Content

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -7,21 +6,14 @@ class App extends Component {
     super();
 
     this.state = {
-      monsters: [
-        {
-          id: 'm1',
-          name: 'Frankenstein'
-        },
-        {
-          id: 'm2',
-          name: 'Dracula'
-        },
-        {
-          id: 'm3',
-          name: 'Zombie'
-        }
-      ]
-    }
+      monsters: []
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({ monsters: users }));
   }
 
   render() {
