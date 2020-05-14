d7135ea12ea8c95570605bd86dd67beb67c860a5
Ryan Moradas Viajedor <viajedor.ryan1990@gmail.com>
2020-05-14 15:11:04 +0800

    Updates

---
+++ precache-manifest.795a4909d513a338e659e9474365eca8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81094ef313b08ed999e15088e854ad1b",
+    "url": "/ryanmoradasviajedor/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "b6700fbe1ed9dfa62090",
+    "url": "/ryanmoradasviajedor/monsters-rolodex/static/css/main.4c94e01f.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/ryanmoradasviajedor/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ryanmoradasviajedor/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6700fbe1ed9dfa62090",
+    "url": "/ryanmoradasviajedor/monsters-rolodex/static/js/main.38e789ef.chunk.js"
+  },
+  {
+    "revision": "9c7e81cf4dbd2b0bafa4",
+    "url": "/ryanmoradasviajedor/monsters-rolodex/static/js/runtime-main.0687e096.js"
+  }
+]);
